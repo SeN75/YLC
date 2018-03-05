c04c4a3859e9ae59cd7045c0d0c3bad7f5f9180c
sen75 <alsaggaf@qleanapp.com>
2018-03-05 22:50:26 +0300

    nav bar , l values and we proud

--- YLC-Project/assets/js/navbar.js
+++ YLC-Project/assets/js/navbar.js
@@ -1,16 +1,29 @@
 window.onscroll = function () {
-	scrollFunction()
+    scrollFunction()
 };
 
 function scrollFunction() {
-	if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-$('#navbar').addClass("b")		
-	} else {
-		document.getElementById("navbar").style.top = "-120px";
-	}
-	if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-		document.getElementById("navbar2").style.top = "0";
-	} else {
-		document.getElementById("navbar2").style.top = "-120px";
-	}
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        $('.navbar').removeClass("bColor-T");
+         $('.navbar').addClass("bColor-0");
+                $('#navbar-menu').removeClass("margin-top--100");
+
+    }
+    else {
+       
+         $('.navbar').removeClass("bColor-0");
+        $('.navbar').addClass("bColor-T");
+        $('#navbar-menu').addClass("margin-top--100");
+    }
+//    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//        document.getElementById("navbar2").style.top = "0";
+//    }
+//    else {
+//        document.getElementById("navbar2").style.top = "-120px";
+//    }
 }
+$(document).ready(function(){
+    $(".navbar-toggler").click(function(){
+       $('#navbar-menu').removeClass("margin-top--100");
+    });
+});
